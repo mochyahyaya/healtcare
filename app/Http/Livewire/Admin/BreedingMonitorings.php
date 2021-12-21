d9abb159ff7bddac344b2573a1c892cee0f81d76
Muhammad Yahya <mochyahyaya@gmail.com>
2021-12-21 14:25:29 +0700

    enchance(monitoring): create breeding monitoring, edit dashboard user

---
+++ app/Http/Livewire/Admin/BreedingMonitorings.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+use Livewire\WithPagination;
+use Livewire\WithFileUploads;
+use App\Models\MonitoringBreeding;
+
+class BreedingMonitorings extends Component
+{
+    use WithPagination,WithFileUploads;
+
+    public $enumfood          = ['Normal', 'Tidak Normal'];
+    public $enumtemperature   = ['Normal', 'Tidak Normal'];
+    public $enummedicine      = ['Sudah', 'Tidak Perlu'];
+    public $food, $temperature, $medicine, $notes, $hotel_id;
+    public $photo = [];
+
+    public function mount($id)
+    {
+        $this->hotel_id = $id;
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'food'              => 'required',
+            'temperature'       => 'required',
+            'medicine'          => 'required',
+            'notes'             => 'required',
+            'photo.*'           => 'required',
+        ]);
+
+        if (!empty($this->photo)){
+            foreach($this->photo as $key=>$galeries){
+                $this->photo[$key] = $galeries->store('public/boardmonitoring');
+            }
+        }
+        $this->photo = json_encode($this->photo); 
+        MonitoringBreeding::create([
+            'food'          => $this->food,
+            'hotel_id'      => $this->hotel_id,
+            'temperature'   => $this->temperature,
+            'medicine'      => $this->medicine,
+            'notes'         => $this->notes,
+            'photo'         => $this->photo,
+        ]); 
+    }
+
+    public function read()
+    {
+        return MonitoringBreeding::all();
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.breeding-monitorings');
+    }
+}
