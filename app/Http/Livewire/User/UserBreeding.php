20b1c3f93f1a6585d232358661e011c31bdafaa0
Muhammad Yahya <mochyahyaya@gmail.com>
2022-01-10 00:23:16 +0700

    enhance(user):cr breeding, boarding, contactus tbl

--- app/Http/Livewire/User/UserBreeding.php
+++ app/Http/Livewire/User/UserBreeding.php
@@ -25,9 +25,12 @@ class UserBreeding extends Component
         Breeding::create([ 
             'pet_id_1'          => $this->pet1,
             'pet_id_2'          => $this->pet2,
-            'status'            => 'belum diproeses',
+            'status'            => 'belum diproses',
             'start_date'        => $this->start_date,
         ]);
+
+        session()->flash('success', 'Berhasil mendaftarkan boarding.');
+        return redirect()->to('/user/dashboard');
     }
     public function pet()
     {
@@ -35,13 +38,13 @@ class UserBreeding extends Component
     }
     public function pets2()
     {
-        return Pet::where('user_id', '1')->get();
+        return (Pet::where('user_id', '1')->get());
     }
     public function render()
     {
         return view('livewire.user.user-breeding', [
             'pet'   => $this->pet(),
-            'pet2'  => $this->pets2()
-        ]);
+            'pets2'  => $this->pets2()
+        ])->extends('layouts.user')->section('main');;
     }
 }
