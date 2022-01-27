ab3a76915509fa659dc477466781aefdd3eb8248
Muhammad Yahya <mochyahyaya@gmail.com>
2022-01-27 09:24:18 +0700

    fix confict

--- resources/views/livewire/user/user-grooming.blade.php
+++ resources/views/livewire/user/user-grooming.blade.php
@@ -71,37 +71,41 @@
     </div>
 		<form class="bg-white p-10 rounded-lg shadow-lg min-w-full mt-4" wire:submit.prevent="store">
 			<h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Pendaftaran Grooming</h1>
-			<div wire:model="pet_id">
-				<label class="text-gray-800 font-semibold block my-3 text-md" for="username">Nama Hewan Peliharaan</label>
-				<select class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none">
-          <option selected>Pilih Pet</option>
-          @foreach ($pet as $item)
-              <option value="{{$item->id}}"> {{$item->name}} </option>
-          @endforeach
-        </select>
-      </div>
-      <div wire:model="service">
-        <label class="text-gray-800 font-semibold block my-3 text-md" for="email">Jenis Grooming</label>
+        <div wire:model="pet_id">
+          <label class="text-gray-800 font-semibold block my-3 text-md" for="username">Nama Hewan Peliharaan</label>
           <select class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none">
-            <option selected>Jenis Grooming</option>
-            <option value="standar">Standar</option>
-            <option value="Jamur">Jamur</option>
-            <option value="Kutu">Kutu</option>
+            <option selected>Pilih Pet</option>
+            @foreach ($pet as $item)
+                <option value="{{$item->id}}"> {{$item->name}} </option>
+            @endforeach
           </select>
         </div>
-        <div wire:model="size">
-          <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Ukuran</label>
-          <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="" />
-            <p class="mt-2 text-blue-800 text-sm">
-              Ukuran dalam bentuk S, M , L.
-            </p>
+        <div wire:model="service">
+          <label class="text-gray-800 font-semibold block my-3 text-md" for="email">Jenis Grooming</label>
+            <select class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none">
+              <option selected>Jenis Grooming</option>
+              <option value="standar">Standar</option>
+              <option value="Jamur">Jamur</option>
+              <option value="Kutu">Kutu</option>
+            </select>
+          </div>
+          <div wire:model="size">
+            <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Ukuran</label>
+            <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="" />
+              <p class="mt-2 text-blue-800 text-sm">
+                Ukuran dalam bentuk S, M , L.
+              </p>
+          </div>
+          <div wire:model="address">
+            <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Alamat</label>
+            <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="confirm" id="confirm" placeholder="" />
+          </div>
+          <div class="mt-4">
+            <button type="submit" class="mt-6 bg-blue-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">
+              Grooms
+            </button>
         </div>
-        <div wire:model="address">
-          <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Alamat</label>
-          <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="confirm" id="confirm" placeholder="" />
-        </div>
-							<button type="submit" class="mt-6 bg-blue-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Grooms</button>
-		</form>
+		  </form>
 	</div>
 </div>
 >>>>>>> c66733d (enchance(user): add template, crud)
